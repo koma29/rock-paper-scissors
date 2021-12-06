fb71ce945e4c4fc48c58ecb20bec6ea252990365
Cody Marshall <codywademarshall@gmail.com>
2021-12-05 21:27:45 -0800

    further tried to fix the game function, not looping 5 times.

--- scripts.js
+++ scripts.js
@@ -4,42 +4,54 @@ function computerPlay() {
     return (random, choice[random]);
 }
 
-function playRound(playerSelection, computerSelection) {
-    computerSelection = computerPlay();
-    playerSelection = prompt("Pick a choice", "");
+function playRound() {
+    let computerSelection = computerPlay();
+    let playerSelection = prompt("Pick a choice", "");
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    console.log(playerSelection, computerSelection);
     if(playerSelection == "Rock" && computerSelection == "Paper") {
         return "You Lose! Paper beats rock.";
     } else if(playerSelection == "Rock" && computerSelection == "Scissors") {
         return "You Win! Rock beats scissors.";
     } else if(playerSelection == "Rock" && computerSelection == "Rock") {
-        return "Tie."
+        return "Tie.";
     } else if(playerSelection == "Scissors" && computerSelection == "Paper") {
         return "You Win! Scissors beats paper.";
     } else if(playerSelection == "Scissors" && computerSelection == "Rock") {
         return "You Lose! Rock beats scissors.";
     } else if(playerSelection == "Scissors" && computerSelection == "Scissors") {
-        return "Tie."
+        return "Tie.";
     } else if(playerSelection == "Paper" && computerSelection == "Scissors") {
         return "You Lose! Scissors beats paper.";
     } else if(playerSelection == "Paper" && computerSelection == "Rock") {
         return "You Win! Paper beats rock.";
     } else  if(playerSelection == "Paper" && computerSelection == "Paper") {
-        return "Tie."
+        return "Tie.";
     } else {
-        return "That is not a choice."
+        return "That is not a choice.";
     }
 }
 
 function game() {
+    let playerScore = 0;
+    let computerScore = 0;
     for(let step = 0; step < 5; step++) {
         playRound();
-        // if() {
-        //     playRound()
-        // } else {
-
-        // }
+        if(playRound().includes("You Win!")) {
+            playerScore++;
+        } else if(playRound().includes("You Lose!")) {
+            computerScore++;
+        } else {
+            console.log("Tie");
+        }
+        console.log(playerScore, computerScore);
+        if(playerScore > computerScore) {
+            return "You Won!";
+        } else if(computerScore > playerScore) {
+            return "You Lost!"
+        } else {
+            return "Draw"
+        }
     }
-    console.log("no");
 }
