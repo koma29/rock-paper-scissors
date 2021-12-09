076d95c205a3efbedfb6f6d9463ff48d195cbf62
Cody Marshall <codywademarshall@gmail.com>
2021-12-08 18:59:30 -0800

    Rock, paper, scissors is now fully working from the console. Also began adding an interactive HTML for future changes.

--- scripts.js
+++ scripts.js
@@ -9,24 +9,24 @@ function playRound() {
     let playerSelection = prompt("Pick a choice", "");
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    console.log(playerSelection, computerSelection);
-    if(playerSelection == "Rock" && computerSelection == "Paper") {
+    console.log("Player Choice:", playerSelection,"|", "Computer Choice:", computerSelection);
+    if(playerSelection === "Rock" && computerSelection === "Paper") {
         return "You Lose! Paper beats rock.";
-    } else if(playerSelection == "Rock" && computerSelection == "Scissors") {
+    } else if(playerSelection === "Rock" && computerSelection === "Scissors") {
         return "You Win! Rock beats scissors.";
-    } else if(playerSelection == "Rock" && computerSelection == "Rock") {
+    } else if(playerSelection === "Rock" && computerSelection === "Rock") {
         return "Tie.";
-    } else if(playerSelection == "Scissors" && computerSelection == "Paper") {
+    } else if(playerSelection === "Scissors" && computerSelection === "Paper") {
         return "You Win! Scissors beats paper.";
-    } else if(playerSelection == "Scissors" && computerSelection == "Rock") {
+    } else if(playerSelection === "Scissors" && computerSelection === "Rock") {
         return "You Lose! Rock beats scissors.";
-    } else if(playerSelection == "Scissors" && computerSelection == "Scissors") {
+    } else if(playerSelection === "Scissors" && computerSelection === "Scissors") {
         return "Tie.";
-    } else if(playerSelection == "Paper" && computerSelection == "Scissors") {
+    } else if(playerSelection === "Paper" && computerSelection === "Scissors") {
         return "You Lose! Scissors beats paper.";
-    } else if(playerSelection == "Paper" && computerSelection == "Rock") {
+    } else if(playerSelection === "Paper" && computerSelection === "Rock") {
         return "You Win! Paper beats rock.";
-    } else  if(playerSelection == "Paper" && computerSelection == "Paper") {
+    } else  if(playerSelection === "Paper" && computerSelection === "Paper") {
         return "Tie.";
     } else {
         return "That is not a choice.";
@@ -36,22 +36,25 @@ function playRound() {
 function game() {
     let playerScore = 0;
     let computerScore = 0;
-    for(let step = 0; step < 5; step++) {
-        playRound();
-        if(playRound().includes("You Win!")) {
+    for(i=0; i<5; i++) {
+        let result = playRound();
+        if(result.includes("You Win!")) {
             playerScore++;
-        } else if(playRound().includes("You Lose!")) {
+        } else if(result.includes("You Lose!")) {
             computerScore++;
+        } else if(result.includes("That is not a choice.")) {
+            i--
+            console.log("That is not a choice. Try again.");
         } else {
             console.log("Tie");
         }
-        console.log(playerScore, computerScore);
-        if(playerScore > computerScore) {
-            return "You Won!";
-        } else if(computerScore > playerScore) {
-            return "You Lost!"
-        } else {
-            return "Draw"
-        }
+        console.log(playerScore, "Player Score", computerScore, "Computer Score");
+    }
+    if(playerScore > computerScore) {
+        return "You Won!";
+    } else if(computerScore > playerScore) {
+        return "You Lost!"
+    } else {
+        return "Draw"
     }
 }
